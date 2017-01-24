97ac12d8c8c7c17163f6d373ae56dbb20c16333e
dave <david.stirrat.2013@uni.strath.ac.uk>
2017-01-24 14:06:59 +0000

    Progressing through login and registration functionality

---
+++ app/src/main/java/app/configuration.java
@@ -0,0 +1,13 @@
+package app;
+
+/**
+ * Created by Dave on 24/01/2017.
+ */
+
+public class configuration {
+    // The url used for logging in to the server
+    public static String login = "http://86.132.127.153/my_login_api/login.php";
+
+    // The url used for registering
+    public static String register = "http://86.132.127.153/my_login_api/register.php";
+}
